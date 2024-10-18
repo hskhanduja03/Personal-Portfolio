7a88c1fd227e4f90b3c3b56576d38da87d52c6c3
Harmeet Singh <128402172+hskhanduja03@users.noreply.github.com>
2024-10-18 20:00:17 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Allows access from external IPs
+    port: 5173, // Port number
+    strictPort: true // Ensures the port won't change if 5173 is already in use
+  },
 })
